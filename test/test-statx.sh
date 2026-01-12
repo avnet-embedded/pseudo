b241b5d5c95269e0ab2567f5f968ff6ac13c68d6
Mark Hatle <mark.hatle@kernel.crashing.org>
2026-01-12 16:06:28 -0600

    test/test-statx.sh: It should be a failure if pseudo prints an error
    Capture the output and compare to a know error message, error if
    the null message is present.
    
    This is currently a known failure, reported in Yocto Project bug 16075.
    
    Signed-off-by: Mark Hatle <mark.hatle@kernel.crashing.org>

--- test/test-statx.sh
+++ test/test-statx.sh
@@ -3,4 +3,16 @@
 # SPDX-License-Identifier: LGPL-2.1-only
 #
 
-exec ./test/test-statx
+tmplog="$(mktemp pseudo.log.XXXXXXXX)"
+./test/test-statx >${tmplog} 2>&1
+rc=$?
+
+if grep -q "couldn't allocate absolute path for 'null'." ${tmplog} ; then
+    echo "Unexpected message: \"couldn't allocate absolute path for 'null'.\" found."
+    rc=1
+fi
+
+cat ${tmplog}
+
+rm $tmplog
+exit $rc
