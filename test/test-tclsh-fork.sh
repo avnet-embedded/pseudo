d3533863064b3cad9151073a68b6da1efe4ab2ee
Mark Hatle <mark.hatle@kernel.crashing.org>
2026-01-14 11:57:09 -0600

    test-tclsh-fork: Skip test if tclsh is not available
    Signed-off-by: Mark Hatle <mark.hatle@kernel.crashing.org>

--- test/test-tclsh-fork.sh
+++ test/test-tclsh-fork.sh
@@ -3,6 +3,11 @@
 # SPDX-License-Identifier: LGPL-2.1-only
 #
 
+which tclsh >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+	exit 255
+fi
+
 # Check that tclsh doesn't hang.  Note that the timeout is not needed to
 # reproduce the hang in tclsh, it's only there to ensure that this test script
 # doesn't hang in case of a failing test.
